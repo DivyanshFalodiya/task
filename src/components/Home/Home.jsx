11369a7a572e3be481dd68a68b0ab32ddbd8be9c
Divyans Falodiya <divyanshfalodiyaab@gmail.com>
2022-03-23 15:17:48 +0530

    :lipstick: subtle UI changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ import {
   CardMedia,
   Grid,
   Typography,
+  useTheme,
 } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { Add, Edit, ViewList } from "@mui/icons-material";
@@ -28,43 +29,56 @@ const useStyles = makeStyles((theme) => ({
 // Home Page
 const Home = () => {
   const classes = useStyles();
+  const theme = useTheme();
+
+  // Helpers
+  const cards = [
+    { path: "/view", text: "View", Icon: ViewList },
+    { path: "/create", text: "Create", Icon: Edit },
+  ];
+
   return (
     <Box
       sx={{
         width: "100%",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
       }}
     >
-      <Grid container display="flex" alignItems="center">
-        <Grid item xs={12} sm={6} className={classes.gridItems}>
-          <Link to="/view" className={classes.link}>
-            <Card>
-              <CardActionArea sx={{ p: 2 }}>
-                <CardMedia
-                  component={ViewList}
-                  sx={{ height: 100, width: 100 }}
-                />
-                <Typography align="center" variant="h5">
-                  View
-                </Typography>
-              </CardActionArea>
-            </Card>
-          </Link>
-        </Grid>
-        <Grid item xs={12} sm={6} className={classes.gridItems}>
-          <Link to="/create" className={classes.link}>
-            <Card>
-              <CardActionArea sx={{ p: 2 }}>
-                <CardMedia component={Edit} sx={{ height: 100, width: 100 }} />
-                <Typography align="center" variant="h5">
-                  Create
-                </Typography>
-              </CardActionArea>
-            </Card>
-          </Link>
-        </Grid>
+      <Typography
+        align="center"
+        variant="h3"
+        color={theme.palette.text.primary}
+        sx={{ fontFamily: "Montserrat, Roboto, sans-serif" }}
+      >
+        UNOMOK
+      </Typography>
+      <Grid
+        container
+        display="flex"
+        alignItems="center"
+        sx={{ mt: theme.spacing(5) }}
+      >
+        {cards.map((c, i) => (
+          <Grid key={i} item xs={12} sm={6} className={classes.gridItems}>
+            <Link to={c.path} className={classes.link}>
+              <Card
+                sx={{
+                  background: theme.palette.background.default,
+                  border: `1px solid ${theme.palette.divider}`,
+                }}
+              >
+                <CardActionArea sx={{ p: 2 }}>
+                  <CardMedia
+                    component={c.Icon}
+                    sx={{ height: 100, width: 100 }}
+                  />
+                  <Typography align="center" variant="h5">
+                    {c.text}
+                  </Typography>
+                </CardActionArea>
+              </Card>
+            </Link>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
