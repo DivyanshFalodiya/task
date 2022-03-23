03c06d9cf5a10519c0951087519d7a4d2a76ca19
Divyans Falodiya <divyanshfalodiyaab@gmail.com>
2022-03-23 15:30:32 +0530

    :bug: fixed the basename

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <ThemeModeProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/task">
         <MainComponent />
       </BrowserRouter>
     </ThemeModeProvider>
